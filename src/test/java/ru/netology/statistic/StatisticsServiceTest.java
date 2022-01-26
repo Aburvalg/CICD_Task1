dc10ae6fda8bc030508645d6bdcc6fb7115b768c
Andrey <3451z@mail.ru>
2022-01-26 09:29:38 +0300

    Added new tests

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -7,13 +7,61 @@ import static org.junit.jupiter.api.Assertions.*;
 class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+    void findMaxStValue() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 0, 8, 6, 11, 11, 12};
         long expected = 12;
 
         long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxAllNegative() {
+        StatisticsService service = new StatisticsService();
+
+        long[] i = {-12, -5, -8, -4, -5, -3, -3, -6, -11, -11, -12};
+        long expected = -3;
+
+        long actual = service.findMax(i);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxNegativeAndPositive() {
+        StatisticsService service = new StatisticsService();
+
+        long[] i = {-12, 5, 8, 4, -5, 0, 8, 6, -11, 11, -12};
+        long expected = 11;
+
+        long actual = service.findMax(i);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxBiggestValue() {
+        StatisticsService service = new StatisticsService();
+
+        long[] i = {-125745, 557457, 8457457, 457457, -5457457, 3346547, 85757, 657574, -1157457, 115754754, -12756574};
+        long expected = 115754754;
+
+        long actual = service.findMax(i);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxTheSameValue() {
+        StatisticsService service = new StatisticsService();
+
+        long[] i = { 1, 1, 1, 1, 1, 1, 1};
+        long expected = 1;
+
+        long actual = service.findMax(i);
 
         assertEquals(expected, actual);
     }
